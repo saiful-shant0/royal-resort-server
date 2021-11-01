6ade77cdfe060773c6339407adf81768999f6551
saiful-shant0 <sshanto.cse@gmail.com>
2021-11-01 19:49:12 +0600

    1

---
+++ index.js
@@ -0,0 +1,75 @@
+const express = require('express');
+const { MongoClient } = require('mongodb');
+
+const ObjectId = require('mongodb').ObjectId;
+
+const cors = require('cors');
+require('dotenv').config();
+const app = express();
+const port = process.env.PORT || 5000;
+app.use(cors());
+app.use(express.json());
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.med0q.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+
+console.log(uri)
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+
+
+async function run() {
+    try {
+        await client.connect();
+        const database = client.db('Resort_Services');
+        const serviceCollection = database.collection('Services');
+        console.log('database connected');
+
+        app.get('/services', async (req, res) => {
+            const cursor = serviceCollection.find({});
+            const services = await cursor.toArray();
+            res.send(services);
+        });
+
+        // GET Single Service
+        app.get('/services/:id', async (req, res) => {
+            const id = req.params.id;
+            console.log('getting specific service', id);
+            const query = { _id: ObjectId(id) };
+            const service = await serviceCollection.findOne(query);
+            res.json(service);
+        })
+
+        app.post('/services', async (req, res) => {
+            const service = req.body;
+            console.log('hit the post api', service);
+
+            const result = await serviceCollection.insertOne(service);
+            console.log(result);
+            res.json(result)
+        });
+
+        app.delete('/services/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) };
+            const result = await serviceCollection.deleteOne(query);
+
+            console.log('deleting user with id ', result);
+
+            res.json(result);
+        })
+
+
+    }
+    finally {
+        // await client.close()
+    }
+
+}
+run().catch(console.dir);
+
+app.get('/', (req, res) => {
+    res.send('Ema jon server is running');
+})
+
+app.listen(port, () => {
+    console.log('Server running at port', port)
+})
