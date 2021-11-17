c1dc533f9c09b2647f53d55683a8db362bfd7afe
saiful-shant0 <sshanto.cse@gmail.com>
2021-11-18 00:30:18 +0600

    final

--- index.js
+++ index.js
@@ -31,7 +31,14 @@ async function run() {
             res.send(services);
         });
 
+        // GET Single Service
+        app.get('/services/:id', async (req, res) => {
+            const id = req.params.id;
 
+            const query = { _id: ObjectId(id) };
+            const service = await serviceCollection.findOne(query);
+            res.json(service);
+        })
 
 
         //ADD order Api
